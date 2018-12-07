c9984bc87fa9852961e1530b4fe2c590bd4281fd
Mateusz Wojtczak <mateusz@leancode.pl>
2018-12-07 01:45:13 +0100

    Add alpha-beta pruning

--- main.dart
+++ main.dart
@@ -20,13 +20,20 @@ Future writeToJson<T>(Node<T> root, String filename) {
 void main(List<String> args) async {
   int players = args.length >= 2 ? int.parse(args[1]) : 2;
 
-  if (args.last == "--maxn") {
+  if (args.contains("--maxn")) {
     Node<List<int>> root = await readFromJson(args[0]);
+
     print(maxn(root, players, 1));
+
     await writeToJson(root, 'result.json');
   } else {
     Node<int> root = await readFromJson(args[0]);
-    print(minimax(root, players, 1));
+
+    if (args.contains("--prune"))
+      print(alphaBeta(root, players, 1, -infinity, infinity));
+    else
+      print(minimax(root, players, 1));
+
     await writeToJson(root, 'result.json');
   }
 }
--- minimax.dart
+++ minimax.dart
@@ -5,6 +5,8 @@ import 'node.dart';
 const infinity = 1000000000;
 
 num minimax(Node<num> node, int players, int playerIndex) {
+  print("visited ${node}");
+
   if (node.terminal) return node.value;
 
   int nextPlayer = (playerIndex + 1) % (players + 1);
@@ -35,4 +37,37 @@ List<num> maxn(Node<List<num>> node, int players, int playerIndex) {
   }
 
   return node.value = best;
+}
+
+num alphaBeta(
+    Node<num> node, int players, int playerIndex, num alpha, num beta) {
+  print("visited ${node}");
+
+  if (node.terminal) return node.value;
+
+  int nextPlayer = (playerIndex + 1) % (players + 1);
+
+  if (playerIndex != 1) {
+    var value = infinity;
+
+    for (var x in node.children) {
+      value = min(value, alphaBeta(x, players, nextPlayer, alpha, beta));
+      beta = min(beta, value);
+
+      if (alpha >= beta) break;
+    }
+
+    return node.value = value;
+  } else {
+    var value = -infinity;
+
+    for (var x in node.children) {
+      value = max(value, alphaBeta(x, players, nextPlayer, alpha, beta));
+      alpha = max(alpha, value);
+
+      if (alpha >= beta) break;
+    }
+
+    return node.value = value;
+  }
 }
